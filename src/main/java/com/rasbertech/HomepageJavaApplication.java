50e69de9dc9d154d54b7ace302579fc06112b252
lys <lys_wyyx@163.com>
2019-01-22 15:51:14 +0800

    first commit

---
+++ src/main/java/com/rasbertech/HomepageJavaApplication.java
@@ -0,0 +1,15 @@
+package com.rasbertech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomepageJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomepageJavaApplication.class, args);
+    }
+
+
+}
+
